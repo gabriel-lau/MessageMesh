294cefbab0b7028155999c15c136973acdce9a23
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2025-02-23 18:41:30 +0700

    count headless

--- backend/ui.go
+++ backend/ui.go
@@ -67,7 +67,7 @@ func UIDataLoop(network Network, ctx context.Context) {
 			case block := <-network.ConsensusService.LatestBlock:
 				debug.Log("ui", "Block: "+block.BlockType)
 			case <-time.After(30 * time.Second):
-				network.SendMessage("Hello I am "+debug.Username+string(count), "Qma9HU4gynWXNzWwpqmHRnLXikstTgCbYHfG6aqJTLrxfq")
+				network.SendMessage("Hello I am "+debug.Username+fmt.Sprint(count), "Qma9HU4gynWXNzWwpqmHRnLXikstTgCbYHfG6aqJTLrxfq")
 				count++
 			case <-ctx.Done():
 				return
