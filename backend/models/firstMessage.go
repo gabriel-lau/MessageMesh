c1d5c216ba2125c0423d18fc61357833bf0b542d
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2025-02-26 22:39:46 +0700

    More firstMessage updates

--- backend/models/firstMessage.go
+++ backend/models/firstMessage.go
@@ -3,6 +3,6 @@ package models
 type FirstMessage struct {
 	PeerID1      string `json:"peerID1"`
 	PeerID2      string `json:"peerID2"`
-	SymetricKey1 string `json:"symetricKey1"`
-	SymetricKey2 string `json:"symetricKey2"`
+	SymetricKey1 string `json:"symetricKey1"` // Symmetric key for peer 1 (Encrypted with peer 1's public key)
+	SymetricKey2 string `json:"symetricKey2"` // Symmetric key for peer 2 (Encrypted with peer 2's public key)
 }
